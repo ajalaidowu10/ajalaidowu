5cfc8de0b73d6385991a07d9791b773749b12e7b
Ajala Idowu <ajalaidowu10@gmail.com>
2022-05-18 20:34:22 +0100

    Add main sections

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import initScrollReveal from "./scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
-
 initScrollReveal(targetElements, defaultProps);
 initTiltEffect();
+import initTiltEffect from "./scripts/tiltAnimation";
+import setYear from "./scripts/myscript";
+setYear();
---
+++ src/script.js
@@ -0,0 +1,8 @@
+export default function setYear(){
+	alert('Hello');
+	console.log('year');
+	// addEventListener('Load', () => {
+	// 	const year = document.querySelector('#year');
+	// 	console.log('year');
+	// });
+}
